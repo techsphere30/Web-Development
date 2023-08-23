453d6e5e426d57477b05ac3870454e9778696728
techsphere30 <techsphere30@gmail.com>
2023-08-22 22:44:47 -0700

    Javascript intro class1

---
+++ JavaScript/script1.js
@@ -0,0 +1,14 @@
+console.log("Welcome to JavaScript");
+// console.log(10 + 2); // Addition
+// console.log(10 - 2); // Subtraction
+// console.log(10 / 2); // Division
+// console.log(10 * 2); // Multiply
+// console.log(15 % 2); // Modulus
+// console.log("Hello" + "to the" + "World"); // Concatenation (joining) of strings together using the '+' operator
+var a = 10; //integer are the numbers without decimal point
+console.log(a)
+var b = 12.5; //floating numbers are the numbers with decimal point
+console.log(b)
+var first = "I am first and my value is 100"; // strings are written in double qoutes
+console.log(first)
+// console.log(a + b + first) 
