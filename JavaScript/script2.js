dd1eb9ce4d42f0347b51e2b7d00c2f1651a86a25
techsphere30 <techsphere30@gmail.com>
2023-08-23 00:46:45 -0700

    javascript datatypes

---
+++ JavaScript/script2.js
@@ -0,0 +1,12 @@
+console.log("Welcome to class 2 of Javascript")
+//data types in JavaScript
+let a = 5; //Number
+let b = false; //boolean
+let c = "500"; //string
+let d; // undefined
+let e = null; //null
+const f = 20; // to declare constants
+a = 1000;
+// f = 2000; // this line generates error because changing the value of a constant is not allowed
+console.log(a, b, c, d, e, f);
+console.log(2 > 10); // generate output false
