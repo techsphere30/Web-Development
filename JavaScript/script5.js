759dd47f95533852eb0e2499f1c508ffd70e3bb8
techsphere30 <techsphere30@gmail.com>
2023-08-30 00:29:26 -0700

    Loops and Arrays

---
+++ JavaScript/script5.js
@@ -0,0 +1,39 @@
+console.log("Welcome to Loops and Arrays");
+// let btn = document.getElementById("button")
+// btn.addEventListener("click", function () {
+//     console.log("mai click hogya");
+// })
+// let age1 = 11;
+// let age2 = 12;
+// let age3 = 13;
+// let age4 = 14;
+// let age5 = 15;
+
+// console.log(age1, age2, age3, age4, age5 );
+
+// let age = [11, 12, 13, 14, 15, "hello", "79"]
+// console.log(age);
+
+
+// there are three types of loops in js
+// 1 For loop
+// 2 While loop
+// 3 do While
+
+// let a = 1;
+// console.log(a++);
+
+// for (let i = 1; i <= 500; i++) {
+//     console.log("Bilal");
+// }
+
+//Escape Sequence
+//1. \n Create New Line
+//2. \' Create Single Qoute
+//3. \" Create Double Qoute
+
+// console.log("hello i am \"ahsan\" and \n i live in \'lahore\'");
+// console.log('2\\6');
+
+// let math = "2+8";
+// console.log(math);
